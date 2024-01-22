cb01e9414972244f42da1b5c7e0a59be806f2843
renata-aline <renatalinec@gmail.com>
2024-01-22 17:57:29 -0300

    base do projeto ph-nutricionista

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+import Banner from "./components/Banner";
+import "./styles/main.sass";
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+const Banner = () => {
+  return (
+    <header className="banner">
+      <img src="/assets/banner.jpeg" />
+      <div className="title">
+        <h1>
+          PH <br /> NUTRICIONISTA
+        </h1>
+        <p>
+          Nutrição Esportiva <br />
+          hipertrofia e emagrecimento.
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Banner;
