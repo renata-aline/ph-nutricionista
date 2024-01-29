236787a9018338226cf7dabbc5d3782cb8e8f191
renata-aline <renatalinec@gmail.com>
2024-01-29 14:51:06 -0300

    adicionado media query

--- src/components/AboutConsultation/index.js
+++ src/components/AboutConsultation/index.js
@@ -2,21 +2,22 @@ const AboutConsultation = () => {
   return (
     <section className="about-consultation">
       <div>
-        <h2>Como Funciona a Consulta</h2>
+        <h2>Como Funciona a Consulta?</h2>
         <video src="./assets/videos/teste.mp4" controls autoPlay muted></video>
       </div>
+      {/* <div className="about-service">
+        <h2>O Que Está Incluso No Meu Atendimento?</h2>
 
-      <h2>O que está incluso no atendimento?</h2>
-      <div className="about-service">
         <ul>
-          <li>Consulta completa</li> 
-          <li>Avaliação corporal</li>  
+          <li>Consulta completa</li>
+          <li>Avaliação corporal</li>
           <li>Planos alimentares individualizados</li>
           <li>Lista de substituição</li>
           <li>Suporte on-line pos consulta</li>
           <li>Entrego seu plano alimentar no dia da consulta</li>
         </ul>
-      </div>
+      </div> */}
+      <h2>Venha Fazer Parte Do Time NUTRY PH!</h2>
     </section>
   );
 };
